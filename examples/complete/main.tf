e904402d0f349506ae993a8e8159503556162aa6
shanye <shanye.xzq@alibaba-inc.com>
2024-01-31 18:20:29 +0800

    add examples for module

---
+++ examples/complete/main.tf
@@ -0,0 +1,57 @@
+#############################################################
+# create managed-kubernetes
+#############################################################
+
+module "managed-k8s" {
+  source  = "terraform-alicloud-modules/managed-kubernetes/alicloud"
+  version = "1.6.0"
+
+  k8s_name_prefix       = "tf-example"
+  cluster_spec          = "ack.pro.small"
+  new_vpc               = true
+  k8s_pod_cidr          = cidrsubnet("10.0.0.0/8", 8, 36)
+  k8s_service_cidr      = cidrsubnet("172.16.0.0/16", 4, 7)
+  worker_instance_types = ["ecs.n1.medium"]
+  kubernetes_version    = "1.24.6-aliyun.1"
+  cluster_addons = [
+    {
+      name   = "flannel",
+      config = "",
+    },
+    {
+      name   = "flexvolume",
+      config = "",
+    },
+    {
+      name   = "alicloud-disk-controller",
+      config = "",
+    },
+    {
+      name   = "logtail-ds",
+      config = "{\"IngressDashboardEnabled\":\"true\"}",
+    },
+    {
+      name   = "nginx-ingress-controller",
+      config = "{\"IngressSlbNetworkType\":\"internet\"}",
+    },
+  ]
+}
+
+data "alicloud_cs_cluster_credential" "auth" {
+  cluster_id                 = module.managed-k8s.this_k8s_id
+  temporary_duration_minutes = 60
+  output_file                = "./config"
+}
+
+
+provider "kubernetes" {
+  config_path = data.alicloud_cs_cluster_credential.auth.output_file
+}
+
+module "k8s" {
+  source = "../.."
+
+  mysql_password = "Abc1234"
+
+  depends_on = [data.alicloud_cs_cluster_credential.auth]
+}
--- wordpress.tf
+++ wordpress.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_service" "wordpress" {
   metadata {
     name = "wordpress"
-    labels {
+    labels = {
       app = "wordpress"
     }
   }
@@ -9,78 +9,91 @@ resource "kubernetes_service" "wordpress" {
     port {
       port = 80
     }
-    selector {
-      app = "wordpress"
-      tier = "${kubernetes_replication_controller.wordpress.spec.0.selector.tier}"
+    selector = {
+      app  = "wordpress"
+      tier = kubernetes_replication_controller.wordpress.spec[0].selector.tier
     }
     type = "LoadBalancer"
+  }
+
+  lifecycle {
+    ignore_changes = ["metadata[0].labels"]
   }
 }
 
 resource "kubernetes_persistent_volume_claim" "wordpress" {
   metadata {
     name = "wp-pv-claim"
-    labels {
+    labels = {
       app = "wordpress"
     }
   }
   spec {
     access_modes = ["ReadWriteOnce"]
     resources {
-      requests {
+      requests = {
         storage = "20Gi"
       }
     }
-    volume_name = "${kubernetes_persistent_volume.wordpress.metadata.0.name}"
+    volume_name = kubernetes_persistent_volume.wordpress.metadata[0].name
   }
 }
 
 resource "kubernetes_replication_controller" "wordpress" {
   metadata {
     name = "wordpress"
-    labels {
+    labels = {
       app = "wordpress"
     }
   }
   spec {
-    selector {
-      app = "wordpress"
+    selector = {
+      app  = "wordpress"
       tier = "frontend"
     }
     template {
-      container {
-        image = "wordpress:${var.wordpress_version}-apache"
-        name  = "wordpress"
-
-        env {
-          name = "WORDPRESS_DB_HOST"
-          value = "wordpress-mysql"
-        }
-        env {
-          name = "WORDPRESS_DB_PASSWORD"
-          value_from {
-            secret_key_ref {
-              name = "${kubernetes_secret.mysql.metadata.0.name}"
-              key = "password"
-            }
-          }
-        }
-
-        port {
-          container_port = 80
-          name = "wordpress"
-        }
-
-        volume_mount {
-          name = "wordpress-persistent-storage"
-          mount_path = "/var/www/html"
+      metadata {
+        labels = {
+          app  = "wordpress"
+          tier = "frontend"
         }
       }
 
-      volume {
-        name = "wordpress-persistent-storage"
-        persistent_volume_claim {
-          claim_name = "${kubernetes_persistent_volume_claim.wordpress.metadata.0.name}"
+      spec {
+        container {
+          image = "wordpress:${var.wordpress_version}-apache"
+          name  = "wordpress"
+
+          env {
+            name  = "WORDPRESS_DB_HOST"
+            value = "wordpress-mysql"
+          }
+          env {
+            name = "WORDPRESS_DB_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.mysql.metadata[0].name
+                key  = "password"
+              }
+            }
+          }
+
+          port {
+            container_port = 80
+            name           = "wordpress"
+          }
+
+          volume_mount {
+            name       = "wordpress-persistent-storage"
+            mount_path = "/var/www/html"
+          }
+        }
+
+        volume {
+          name = "wordpress-persistent-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.wordpress.metadata[0].name
+          }
         }
       }
     }
